2e3425d48f97135dca54c3b7971387896dd8768e
runmaxde <hello@runmax.de>
2021-12-23 21:59:36 +0100

    main container size

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,8 +1,8 @@
-import Navigation from "./Navigation";
+import Navigation from "./Navigation"
 
 export default function Page({ children }) {
   return (
-    <div className="max-w-2xl p-3 m-auto">
+    <div className="max-w-4xl p-3 m-auto">
       <Navigation />
       {children}
     </div>
